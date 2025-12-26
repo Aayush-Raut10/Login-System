96226bf60dbd63fc811daf7b1b9ed98225558adc
Aayush-Raut10 <aayushraut209@gmail.com>
2025-12-26 11:07:57 +0545

    Add signup functionality

---
+++ signup.py
@@ -0,0 +1,23 @@
+import database 
+
+def signupSystem():
+    
+    while True:
+        usr_name = input("Enter Username: ")
+
+        if usr_name in database.user_database:
+            print("This username already exits. Try another.")
+            continue
+
+        password = input("Enter Password: ")
+        confirm_password = input("Confirm Password: ")
+
+        if password != confirm_password:
+            print("Password and confirm password do not match.")
+            continue
+
+        database.user_database[usr_name] = password
+        print("Sign-up completed! You can now log-in.")
+        break
+
+signupSystem()
