ab23ece1c356e8bf65affd1c0c13cad8622a4e7a
Aayush-Raut10 <aayushraut209@gmail.com>
2025-12-27 19:53:13 +0545

    Adding file handling

--- database.py
+++ database.py
@@ -1,6 +1,26 @@
-user_database = {
 
-        'dolindra':"123abc",
-        'aayush':"321abc"
+def load_users():
+    """
+    This function loads the users from file into an in-memory dictionary.
 
-    }
+    """
+    users = {} # dictionary to hold username:password paris
+    try:
+        with open('users.txt', 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line:
+                    username, password = line.split(",")
+                    users[username] = password
+    except FileNotFoundError:
+        pass
+
+    return users
+
+def save_user(username, password):
+    """ Adds a new user to the file """
+
+    with open("users.txt", 'a') as file:
+        file.write(f"{username},{password}\n") #Append new user
+
+user_database = load_users()
--- signup.py
+++ signup.py
@@ -3,20 +3,21 @@ import database
 def signupSystem():
     
     while True:
-        usr_name = input("Enter Username: ")
+        usr_name = input("Enter Username: ").strip()
 
         if usr_name in database.user_database:
             print("This username already exits. Try another.")
             continue
 
-        password = input("Enter Password: ")
-        confirm_password = input("Confirm Password: ")
+        password = input("Enter Password: ").strip()
+        confirm_password = input("Confirm Password: ").strip()
 
         if password != confirm_password:
             print("Password and confirm password do not match.")
             continue
 
         database.user_database[usr_name] = password
+        database.save_user(usr_name,password)
         print("Sign-up completed! You can now log-in.\n")
         break
 
